b9443b605349478431f127dfcb887268f46d3c6a
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-10-23 17:23:30 +0900

    [#38240985] Add image to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name, :price
+  attr_accessible :description, :name, :price, :image, :image_cache
+  mount_uploader :image, ProductImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023072700) do
+ActiveRecord::Schema.define(:version => 20121023080443) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20121023072700) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "price"
+    t.string   "image"
   end
 
 end
