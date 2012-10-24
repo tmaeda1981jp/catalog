ce37b522bb8638b249bc6f52391e86967e5a74d9
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-10-24 14:02:01 +0900

    [#38317593] Shop画像を登録・表示できるようにした

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,4 @@
 class Shop < ActiveRecord::Base
-  attr_accessible :description, :image, :lines_summary, :name
+  attr_accessible :description, :image, :lines_summary, :name, :image_cache
+  mount_uploader :image, ShopImageUploader
 end
