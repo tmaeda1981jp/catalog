70db54dacaf08261d4360f9fce4a5a0262a67f7a
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-10-24 11:17:11 +0900

    [#38240985] priceに独自の検証ロジックを追加した

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,13 @@ class Product < ActiveRecord::Base
   validates_numericality_of :price, greater_than_or_equal_to: 0, only_integer: true
 
   validates_uniqueness_of :name
+
+  validate :price_must_be_a_multiple_of_hundreds
+
+  private
+  def price_must_be_a_multiple_of_hundreds
+    unless price % 100 == 0
+      errors.add :price, "must be a multiple of hundreds"
+    end
+  end
 end
