3144e88355d468d7a0e76a664a82b1fa5c125028
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-10-24 11:12:09 +0900

    [#38240985] priceとnameに検証ロジックを追加した

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
 
   validates_presence_of :name, :description
+
+  # 以下どちらでも良い．下の書き方の方がモダン？ 
+  # validates_numericality_of :price, :greater_than_or_equal_to => 0, :only_integer => true
+  validates_numericality_of :price, greater_than_or_equal_to: 0, only_integer: true
+
+  validates_uniqueness_of :name
 end
