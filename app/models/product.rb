ea782e83bb0bae0d94bc17dbecc4eac7e0d5b85d
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-10-24 10:45:52 +0900

    [#38240985] nameとdescriptionの検証ロジックを追加した

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :name, :price, :image, :image_cache
   mount_uploader :image, ProductImageUploader
+
+  validates_presence_of :name, :description
 end
