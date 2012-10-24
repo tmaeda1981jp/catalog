a0f7dc76e95b3f5551367d068ed58c103e895153
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-10-24 14:05:56 +0900

    [#38317593] Shopモデルに検証ロジックを追加した

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,7 @@
 class Shop < ActiveRecord::Base
   attr_accessible :description, :image, :lines_summary, :name, :image_cache
   mount_uploader :image, ShopImageUploader
+
+  validates_uniqueness_of :name
+  validates_presence_of :name, :description, :lines_summary
 end
