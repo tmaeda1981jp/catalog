ff47b6ebf5629a6e29e86e1c155014458612c5b6
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-10-24 16:26:46 +0900

    [#38241001] decoratorにdisplay_imageとdisplay_descriptionを作って各viewに適用した

--- app/decorators/product_decorator.rb
+++ app/decorators/product_decorator.rb
@@ -1,7 +1,14 @@
 # coding: utf-8
 module ProductDecorator
   def display_price
-    # helperにいる
     number_to_currency(price, unit: '￥', precision: 0)
+  end
+
+  def display_image
+    image_tag(image_url) if image?
+  end
+
+  def display_description
+    simple_format(description)
   end
 end
